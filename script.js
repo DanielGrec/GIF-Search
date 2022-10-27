936626d195d8033a5a20fc8298964e1709ea98ad
Daniel Grec <104867807+DanielGrec@users.noreply.github.com>
2022-10-27 16:29:32 +0300

    Refactored code to use async/await

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ const button = document.querySelector('button');
 let urlNoSearch =
 	'https://api.giphy.com/v1/gifs/translate?api_key=jdaDVbpka0tZrwQoyuiOmMhdbhoVzhtJ&s=';
 
-button.addEventListener('click', () => {
+async function getGifs() {
 	let inputText = input.value;
 	let urlWithSearch = `${urlNoSearch}${inputText}`;
-	fetch(urlWithSearch, { mode: 'cors' })
-		.then((response) => response.json())
-		.then((response) => (img.src = response.data.images.original.url))
-		.catch(() => {
-			img.src =
-				'https://media.giphy.com/media/Vbtc9VG51NtzT1Qnv1/giphy.gif';
-		});
-});
+	const response = await fetch(urlWithSearch, { mode: 'cors' });
+	const gifData = await response.json();
+	img.src = gifData.data.images.original.url;
+}
+
+button.addEventListener('click', getGifs);
+
+// let inputText = input.value;
+// 	let urlWithSearch = `${urlNoSearch}${inputText}`;
+// 	fetch(urlWithSearch, { mode: 'cors' })
+// 		.then((response) => response.json())
+// 		.then((response) => (img.src = response.data.images.original.url))
+// 		.catch(() => {
+// 			img.src =
+// 				'https://media.giphy.com/media/Vbtc9VG51NtzT1Qnv1/giphy.gif';
+// 		});
